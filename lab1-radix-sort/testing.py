ee97f5dda9ba32fd74702c730ce1ee154074a215
Anton Kabanov <klvlwxxw@ya.ru>
2024-11-26 23:07:30 +0300

    lab9 report done

--- lab1-radix-sort/testing.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-import random
-
-for i in range(10**6):
-    randomstring = str(random.getrandbits(64))
-    md5hash = hashlib.md5(randomstring.encode()).hexdigest()
-
-    print(md5hash + '\t' + randomstring)
